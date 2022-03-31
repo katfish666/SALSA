73e9c8ffb6b0dc551f10d1d863959c576cddd101
Kat <kat@cs.unc.edu>
2022-03-31 00:39:23 -0400

    First commit for graph augmentation sprint

---
+++ graph_utils.py
@@ -0,0 +1,49 @@
+import networkx as nx
+from rdkit import Chem
+
+def mol_to_graph(mol):
+    G = nx.Graph()
+
+    for atom in mol.GetAtoms():
+        G.add_node(atom.GetIdx(),
+                   atomic_num=atom.GetAtomicNum(),
+                   formal_charge=atom.GetFormalCharge(),
+                   chiral_tag=atom.GetChiralTag(),
+                   hybridization=atom.GetHybridization(),
+                   num_explicit_hs=atom.GetNumExplicitHs(),
+                   is_aromatic=atom.GetIsAromatic())
+    for bond in mol.GetBonds():
+        G.add_edge(bond.GetBeginAtomIdx(),
+                   bond.GetEndAtomIdx(),
+                   bond_type=bond.GetBondType())
+    return G
+
+def graph_to_mol(G):
+    mol = Chem.RWMol()
+    atomic_nums = nx.get_node_attributes(G, 'atomic_num')
+    chiral_tags = nx.get_node_attributes(G, 'chiral_tag')
+    formal_charges = nx.get_node_attributes(G, 'formal_charge')
+    node_is_aromatics = nx.get_node_attributes(G, 'is_aromatic')
+    node_hybridizations = nx.get_node_attributes(G, 'hybridization')
+    num_explicit_hss = nx.get_node_attributes(G, 'num_explicit_hs')
+    node_to_idx = {}
+    for node in G.nodes():
+        a=Chem.Atom(atomic_nums[node])
+        a.SetChiralTag(chiral_tags[node])
+        a.SetFormalCharge(formal_charges[node])
+        a.SetIsAromatic(node_is_aromatics[node])
+        a.SetHybridization(node_hybridizations[node])
+        a.SetNumExplicitHs(num_explicit_hss[node])
+        idx = mol.AddAtom(a)
+        node_to_idx[node] = idx
+
+    bond_types = nx.get_edge_attributes(G, 'bond_type')
+    for edge in G.edges():
+        first, second = edge
+        ifirst = node_to_idx[first]
+        isecond = node_to_idx[second]
+        bond_type = bond_types[first, second]
+        mol.AddBond(ifirst, isecond, bond_type)
+
+    Chem.SanitizeMol(mol)
+    return mol
